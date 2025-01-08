3676685d277332415fb295e6da4d337a4a3a0bb4
Developer-Galaxy <developer@galaxy-it.net>
2025-01-08 18:07:14 +0200

    feat: papaCarlo start

---
+++ btc-chess.back/resources/js/PapaCarlo/Modules/hero.jsx
@@ -0,0 +1,17 @@
+export default function HeroSection() {
+  return(
+    <div className="global-section hero-section">
+      <div className="container">
+        <div className="hero-heading">
+          <h1 className="hero-title">Carpentry</h1>
+          <div className="hero-heading__info">
+            <div className="hero-heading__description">Lorem ipsum dolor sit amet consectetur. Quis sit tortor vitae semper cras. Pharetra ac elementum dictumst interdum et dolor bibendum. Habitasse pellentesque neque amet et eros donec fermentum. Volutpat felis aenean eu ut auctor quis metus vel aliquam.</div>
+            <div className="hero-heading__btns">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- btc-chess.back/vite.config.js
+++ btc-chess.back/vite.config.js
@@ -10,4 +10,9 @@ export default defineConfig({
         }),
         react(),
     ],
+    // css: {
+    //     preprocessorOptions: {
+    //         scss: {},
+    //     },
+    // },
 });
