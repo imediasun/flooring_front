d66922a3d2f037892d9d8a452d44073402c14926
Vitalii Y <ceo@galaxy-it.net>
2025-01-12 09:17:04 -0500

    fix: hide galaxy meta tags

--- btc-chess.back/resources/views/app.blade.php
+++ btc-chess.back/resources/views/app.blade.php
@@ -12,21 +12,21 @@
 
         <!-- Open Graph meta -->
         <meta property="og:site_name" content="Home" />
-        <meta property="og:title" content="Studio web-development and marketing in USA ⬆️ Galaxy IT" />
-        <meta
-            property="og:description"
-            content="Web-studio Galaxy IT ✔️ SEO website promotion in USA ⭐ Services on creation and development of websites ✔️ SMM promotion in USA ✔️ Order web design services in USA ⭐ Web development studio Galaxy IT ☎️ +1(440)225-4113"
-        />
+{{--        <meta property="og:title" content="Studio web-development and marketing in USA ⬆️ Galaxy IT" />--}}
+{{--        <meta--}}
+{{--            property="og:description"--}}
+{{--            content="Web-studio Galaxy IT ✔️ SEO website promotion in USA ⭐ Services on creation and development of websites ✔️ SMM promotion in USA ✔️ Order web design services in USA ⭐ Web development studio Galaxy IT ☎️ +1(440)225-4113"--}}
+{{--        />--}}
         <meta property="og:locale" content="en" />
         <meta property="og:locale:alternate" content="uk" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://galaxy-it.net/" />
-        <meta property="og:image" content="https://galaxy-it.net/favicon/favicon.png" />
-        <meta property="og:image:secure_url" content="https://galaxy-it.net/favicon/favicon.png" />
-        <meta property="og:image:width" content="32" />
-        <meta property="og:image:height" content="32" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@galaxy_it" />
+{{--        <meta property="og:url" content="https://galaxy-it.net/" />--}}
+{{--        <meta property="og:image" content="https://galaxy-it.net/favicon/favicon.png" />--}}
+{{--        <meta property="og:image:secure_url" content="https://galaxy-it.net/favicon/favicon.png" />--}}
+{{--        <meta property="og:image:width" content="32" />--}}
+{{--        <meta property="og:image:height" content="32" />--}}
+{{--        <meta name="twitter:card" content="summary" />--}}
+{{--        <meta name="twitter:site" content="@galaxy_it" />--}}
 
         <!-- Fix IOS zoom to input on focus -->
         <!-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no , maximum-scale=1.0,user-scalable=no" /> -->
@@ -35,11 +35,11 @@
         <meta content="true" name="HandheldFriendly" />
         <meta content="width" name="MobileOptimized" />
         <meta name="mobile-web-app-capable" content="yes">
-        <meta name="keywords" content="Galaxy IT, SEO website" />
-        <meta
-            name="description"
-            content="Web-studio Galaxy IT ✔️ SEO website promotion in USA ⭐ Services on creation and development of websites ✔️ SMM promotion in USA ✔️ Order web design services in USA ⭐ Web development studio Galaxy IT ☎️ +1(440)225-4113"
-        />
+{{--        <meta name="keywords" content="Galaxy IT, SEO website" />--}}
+{{--        <meta--}}
+{{--            name="description"--}}
+{{--            content="Web-studio Galaxy IT ✔️ SEO website promotion in USA ⭐ Services on creation and development of websites ✔️ SMM promotion in USA ✔️ Order web design services in USA ⭐ Web development studio Galaxy IT ☎️ +1(440)225-4113"--}}
+{{--        />--}}
         <meta name="it-rating" content="it-rat-fa9ef9f8c2095cc94c5d33de60ee9bb3" />
         <meta name="theme-color" content="#ffffff" />
 
